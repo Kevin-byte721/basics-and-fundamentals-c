77b968164bae8768a9e21b74e2c7f0d36f9a3364
Kevin <djk6174@gmail.com>
2025-02-11 13:03:07 +0800

    second commit

--- shoes.cpp
+++ shoes.cpp
@@ -10,52 +10,48 @@ class Shoes {
     string n_price;
     string n_typesofShoes;
     
+    Shoes(string a, string b, string c, string d, string e) {
+    	n_brand = a;
+    	n_size = b;
+    	n_color = c;
+    	n_price = d;
+    	n_typesofShoes = e;
+	}
+    
     string brand;
     int size;
     string color;
     double price;
     string typesofShoes;
     
+    Shoes(string f, int g, string h, double i, string j) {
+    	brand = f;
+    	size = g;
+    	color = h;
+    	price = i;
+    	typesofShoes = j;
+	}
+    
 };
 
 int main() {
-    Shoes shoesobj1;
-    shoesobj1.brand = "Nike" ;
-    shoesobj1.size = 11;
-    shoesobj1.color = "Black" ;
-    shoesobj1.price = 1200.05;
-    shoesobj1.typesofShoes = "Running";
-
-    shoesobj1.n_brand = "Brand: " ;
-    shoesobj1.n_size = "Size: ";
-    shoesobj1.n_color = "Color: " ;
-    shoesobj1.n_price = "Price: ";
-    shoesobj1.n_typesofShoes = "Type of Shoes: ";
-
-    Shoes shoesobj2;
-    shoesobj2.brand = "Adidas";
-    shoesobj2.size = 10;
-    shoesobj2.color = "Gray";
-    shoesobj2.price = 1100.54;
-    shoesobj2.typesofShoes = "Walking";
-
-	shoesobj2.n_brand = "Brand: " ;
-    shoesobj2.n_size = "Size: ";
-    shoesobj2.n_color = "Color: " ;
-    shoesobj2.n_price = "Price: ";
-    shoesobj2.n_typesofShoes = "Type of Shoes: ";
-    
-	cout << shoesobj1.n_brand << shoesobj1.brand << endl;
-	cout << shoesobj1.n_size << shoesobj1.size << endl;
-	cout << shoesobj1.n_color << shoesobj1.color << endl;
-	cout << shoesobj1.n_price << shoesobj1.price << endl;
-	cout << shoesobj1.n_typesofShoes << shoesobj1.typesofShoes << endl << endl; 
+	Shoes shoesobj1("Nike", 11, "Black", 1200.05, "Running");
 	
-	cout << shoesobj2.n_brand << shoesobj2.brand << endl;
-	cout << shoesobj2.n_size << shoesobj2.size << endl;
-	cout << shoesobj2.n_color << shoesobj2.color << endl;
-	cout << shoesobj2.n_price << shoesobj2.price << endl;
-	cout << shoesobj2.n_typesofShoes << shoesobj2.typesofShoes << endl;
+	Shoes shoesobj2("Adidas", 10, "Gray", 1100.54, "Walking"); 
+	
+	Shoes shoesobj("Brand: ", "Size: ", "Color: ", "Price: ", "Types of Shoe: ");
+	
+	cout << shoesobj.n_brand << shoesobj1.brand << endl;
+	cout << shoesobj.n_size << shoesobj1.size << endl;
+	cout << shoesobj.n_color << shoesobj1.color << endl;
+	cout << shoesobj.n_price << shoesobj1.price << endl;
+	cout << shoesobj.n_typesofShoes << shoesobj1.typesofShoes << endl << endl; 
+	
+	cout << shoesobj.n_brand << shoesobj2.brand << endl;
+	cout << shoesobj.n_size << shoesobj2.size << endl;
+	cout << shoesobj.n_color << shoesobj2.color << endl;
+	cout << shoesobj.n_price << shoesobj2.price << endl;
+	cout << shoesobj.n_typesofShoes << shoesobj2.typesofShoes << endl;
 
     return 0;
 }
